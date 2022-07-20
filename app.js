494814093a3297ae78ab47faf8f187a806a8fd9f
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-07-20 08:38:41 +0100

    Basic express server - app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 5000
+const start = async () => {
+    try {
+       app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+start();
