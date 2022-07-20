1736c1bb6ecfbb1dfa7e211cb366e5e9d9bfd722
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-07-20 09:56:28 +0100

    Created a models folder and a user.js with validation from validator.js

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, 'Please provide name'],
+      minlength: 3,
+      maxlength: 40,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Please provide email'],
+      validate: {
+        validator: validator.isEmail,
+        message: 'Please provide a valid email',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  });
+
+  module.exports = mongoose.model('User', UserSchema);
